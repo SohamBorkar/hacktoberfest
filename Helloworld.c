91d26a313ee9d54d2d9ff3423774c1bb1e01323f
SohamBorkar <72187535+SohamBorkar@users.noreply.github.com>
2020-10-01 10:13:17 +0200

    Update Helloworld.c

--- Helloworld.c
+++ Helloworld.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-  printf("Hello World!");
-  printf("You can chanage this for hacktoberfest and learn to make pull requst.");
+  printf("Hello World!\n");
+  printf("You can chanage this for hacktoberfest and learn to make pull requst, also.\n");
   return 0;
 }
